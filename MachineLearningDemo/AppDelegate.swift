be7c50060f7034a444415e716b3652c6305fed32
nsfwow <116341914+nsfwow@users.noreply.github.com>
2020-08-13 20:50:18 +0000

    Update SJ_UI_Guide.rst

--- MachineLearningDemo/AppDelegate.swift
+++ MachineLearningDemo/AppDelegate.swift
@@ -18,3 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         //IQKeyboard Setup
+        IQKeyboardManager.sharedManager().enable = true
+        IQKeyboardManager.sharedManager().enableAutoToolbar = false
+        IQKeyboardManager.sharedManager().shouldShowToolbarPlaceholder = false
+        IQKeyboardManager.s
