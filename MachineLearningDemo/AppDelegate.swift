02ecf41fbfdfaf3f5ef6c5098a253a018fac3ae6
nsfwow <116341914+nsfwow@users.noreply.github.com>
2020-09-03 19:22:32 +0000

    Fix the Qt build.

--- MachineLearningDemo/AppDelegate.swift
+++ MachineLearningDemo/AppDelegate.swift
@@ -37,4 +37,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the 
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started
