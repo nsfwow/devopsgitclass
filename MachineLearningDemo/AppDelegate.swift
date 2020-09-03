b24c8da912bc731b6805ec5789964425f906efe5
nsfwow <116341914+nsfwow@users.noreply.github.com>
2020-09-03 14:41:03 +0000

    1.4.1

--- MachineLearningDemo/AppDelegate.swift
+++ MachineLearningDemo/AppDelegate.swift
@@ -33,4 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background 
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the 
