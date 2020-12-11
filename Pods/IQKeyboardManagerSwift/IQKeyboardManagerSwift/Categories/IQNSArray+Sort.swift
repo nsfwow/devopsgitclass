b78f76b812aa77205278217c96eb5b4904708c14
nsfwow <116341914+nsfwow@users.noreply.github.com>
2020-12-11 05:37:38 +0000

    fixed typo in comments

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/Categories/IQNSArray+Sort.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/Categories/IQNSArray+Sort.swift
@@ -1,2 +1,7 @@
 //
-//  IQNSArray+So
+//  IQNSArray+Sort.swift
+// https://github.com/hackiftekhar/IQKeyboardManager
+// Copyright (c) 2013-16 Iftekhar Qurashi.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated 
