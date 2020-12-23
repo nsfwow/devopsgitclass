1437f46daf4abbc6b814f5be2f0b0899755c181a
nsfwow <116341914+nsfwow@users.noreply.github.com>
2020-12-23 07:51:00 +0000

    ```

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/Categories/IQUIScrollView+Additions.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/Categories/IQUIScrollView+Additions.swift
@@ -1,2 +1,7 @@
 //
-//  
+//  IQUIScrollView+Additions.swift
+// https://github.com/hackiftekhar/IQKeyboardManager
+// Copyright (c) 2013-16 Iftekhar Qurashi.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and 
