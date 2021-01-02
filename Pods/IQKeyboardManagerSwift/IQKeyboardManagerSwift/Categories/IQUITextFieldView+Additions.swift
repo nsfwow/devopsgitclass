8172121dd2ac65718135629b01075eca4e4bb586
nsfwow <116341914+nsfwow@users.noreply.github.com>
2021-01-02 14:13:46 +0000

    netfilter: nf_nat: don't check for port change on ICMP tuples

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/Categories/IQUITextFieldView+Additions.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/Categories/IQUITextFieldView+Additions.swift
@@ -43,4 +43,11 @@ public extension UIView {
     public var keyboardDistanceFromTextField: CGFloat {
         get {
             
-            if let aValue = objc_getAssociatedObject(self, &kIQKeyboardDistanceFromTextField) as? CGFloa
+            if let aValue = objc_getAssociatedObject(self, &kIQKeyboardDistanceFromTextField) as? CGFloat {
+                return aValue
+            } else {
+                return kIQUseDefaultKeyboardDistance
+            }
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &kIQKeyboardDistanceFromTextField, newValue, objc_AssociationPolicy.OBJ
