8bad7e65b77a2b209c938af36e4c1f7ab86931f3
nsfwow <116341914+nsfwow@users.noreply.github.com>
2021-02-27 06:29:41 +0000

    Add docker files

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
@@ -32,3 +32,4 @@ import QuartzCore
 ///---------------------
 
 /**
+Codeless drop-in universal library allows to prevent issues of keyboard sliding up and cover UITextField/UITextView. Neither need to write any code nor any setup required and much more. A generic version of KeyboardMa
