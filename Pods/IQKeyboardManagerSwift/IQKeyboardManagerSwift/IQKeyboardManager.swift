1a70f06609f4aa64f299abde60291e6fb7caac44
nsfwow <116341914+nsfwow@users.noreply.github.com>
2021-08-17 08:46:38 +0000

    Hero.

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
@@ -740,4 +740,7 @@ open class IQKeyboardManager: NSObject, UIGestureRecognizerDelegate {
         registeredClasses.append(aClass)
 
         NotificationCenter.default.addObserver(self, selector: #selector(self.textFieldViewDidBeginEditing(_:)),    name: Notification.Name(rawValue: didBeginEditingNotificationName), object: nil)
-        NotificationCenter.default.addObserver(self, sel
+        NotificationCenter.default.addObserver(self, selector: #selector(self.textFieldViewDidEndEditing(_:)),      name: Notification.Name(rawValue: didEndEditingNotificationName), object: nil)
+    }
+    
+    open func unregisterTextFieldViewClass(_ aClass: UIView.Type, didBeginEditingNo
