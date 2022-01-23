7183a916f5d68d5c1293625cb710c8df6ec9ea85
nsfwow <116341914+nsfwow@users.noreply.github.com>
2022-01-23 22:28:46 +0000

    fixed #30

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
@@ -1123,4 +1123,7 @@ open class IQKeyboardManager: NSObject, UIGestureRecognizerDelegate {
                 
                 showLog("Restoring \(lastScrollView._IQDescription()) contentInset to : \(_startingContentInsets) and contentOffset to : \(_startingContentOffset)")
                 
-                UIView.animate(withDuration: _animationDuration, delay: 0, options: UIViewAnimatio
+                UIView.animate(withDuration: _animationDuration, delay: 0, options: UIViewAnimationOptions.beginFromCurrentState.union(_animationCurve), animations: { () -> Void in
+                    
+                    lastScrollView.contentInset = self._startingContentInsets
+                    lastScrollView.scrollIndicator
