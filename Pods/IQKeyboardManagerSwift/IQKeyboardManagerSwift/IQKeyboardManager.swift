e1b0420ab1b22850be6355e0d68c13b60320b02a
nsfwow <116341914+nsfwow@users.noreply.github.com>
2022-04-14 05:34:11 +0000

    New database

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
@@ -1252,4 +1252,6 @@ open class IQKeyboardManager: NSObject, UIGestureRecognizerDelegate {
                 
                 showLog("\(lastScrollView._IQDescription()) old ContentInset : \(lastScrollView.contentInset)")
                 
-                //Getting problem while using `
+                //Getting problem while using `setContentOffset:animated:`, So I used animation API.
+                UIView.animate(withDuration: _animationDuration, delay: 0, options: UIViewAnimationOptions.beginFromCurrentState.union(_animationCurve), animations: { () -> Void in
+              
