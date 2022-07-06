df680aa33971c5d61bbcb3c850ac9471720e402a
nsfwow <116341914+nsfwow@users.noreply.github.com>
2022-07-06 20:57:53 +0000

    merge lp:juju-core/trunk

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQKeyboardManager.swift
@@ -1536,4 +1536,7 @@ open class IQKeyboardManager: NSObject, UIGestureRecognizerDelegate {
         //If last restored keyboard size is different(any orientation accure), then refresh. otherwise not.
         if _kbSize.equalTo(oldKBSize) == false {
             
-            //If _textFieldView is inside UITableViewController then let UITableViewController to handle it (Bug ID: #37) (Bug ID: #76) See note:- https://developer.apple.com/library/ios/documentation/StringsTextF
+            //If _textFieldView is inside UITableViewController then let UITableViewController to handle it (Bug ID: #37) (Bug ID: #76) See note:- https://developer.apple.com/library/ios/documentation/StringsTextFonts/Conceptual/TextAndWebiPhoneOS/KeyboardManagement/KeyboardManagement.html If it is UIAlertView textField then do not affect anything (Bug ID: #70).
+            
+            if _privateIsKeyboardShowing == true &&
+                _textF
