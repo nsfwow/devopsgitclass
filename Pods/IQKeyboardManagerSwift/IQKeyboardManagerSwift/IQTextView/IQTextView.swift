61450ade204bfb1e9c14800a66ba1818a36a1ec8
nsfwow <116341914+nsfwow@users.noreply.github.com>
2022-12-24 13:50:03 +0000

    x86/microcode/amd: Tone down printk(), don't treat a missing firmware file as an error

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQTextView/IQTextView.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQTextView/IQTextView.swift
@@ -1,4 +1,8 @@
 //
 //  IQTextView.swift
 // https://github.com/hackiftekhar/IQKeyboardManager
-// Co
+// Copyright (c) 2013-16 Iftekhar Qurashi.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including with
