952e836c23463152f8daf844965323256ecf87e9
nsfwow <116341914+nsfwow@users.noreply.github.com>
2023-01-09 06:42:33 +0000

    Prepare 0.4.3 release

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQToolbar/IQBarButtonItem.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQToolbar/IQBarButtonItem.swift
@@ -1,3 +1,7 @@
 //
 //  IQBarButtonItem.swift
-/
+// https://github.com/hackiftekhar/IQKeyboardManager
+// Copyright (c) 2013-16 Iftekhar Qurashi.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
