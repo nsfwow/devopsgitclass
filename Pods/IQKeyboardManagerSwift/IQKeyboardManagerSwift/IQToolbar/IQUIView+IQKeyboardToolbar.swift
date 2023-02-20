84c1500abcd4a384e0dacd9e0fe77e209999f0c6
nsfwow <116341914+nsfwow@users.noreply.github.com>
2023-02-20 05:27:22 +0000

    update snapshot

--- Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQToolbar/IQUIView+IQKeyboardToolbar.swift
+++ Pods/IQKeyboardManagerSwift/IQKeyboardManagerSwift/IQToolbar/IQUIView+IQKeyboardToolbar.swift
@@ -50,3 +50,241 @@ private var kIQToolbarPlaceholder           = "kIQToolbarPlaceholder"
 private var kIQKeyboardToolbar              = "kIQKeyboardToolbar"
 
 /**
+UIView category methods to add IQToolbar on UIKeyboard.
+*/
+public extension UIView {
+    
+    /**
+     IQToolbar references for better customization control.
+     */
+    public var keyboardToolbar: IQToolbar {
+        get {
+            var toolbar = inputAccessoryView as? IQToolbar
+            
+            if (toolbar == nil)
+            {
+                toolbar = objc_getAssociatedObject(self, &kIQKeyboardToolbar) as? IQToolbar
+            }
+            
+            if let unwrappedToolbar = toolbar {
+                
+                return unwrappedToolbar
+
+            } else {
+                
+                let newToolbar = IQToolbar()
+                
+                objc_setAssociatedObject(self, &kIQKeyboardToolbar, newToolbar, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+
+                return newToolbar
+            }
+        }
+    }
+    
+    ///-------------------------
+    /// MARK: Title
+    ///-------------------------
+    
+    /**
+    If `shouldHideToolbarPlaceholder` is YES, then title will not be added to the toolbar. Default to NO.
+    */
+    public var shouldHideToolbarPlaceholder: Bool {
+        get {
+            let aValue = objc_getAssociatedObject(self, &kIQShouldHideToolbarPlaceholder) as Any?
+            
+            if let unwrapedValue = aValue as? Bool {
+                return unwrapedValue
+            } else {
+                return false
+            }
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &kIQShouldHideToolbarPlaceholder, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            
+            self.keyboardToolbar.titleBarButton.title = self.drawingToolbarPlaceholder
+        }
+    }
+
+    @available(*,deprecated, message: "This is renamed to `shouldHideToolbarPlaceholder` for more clear naming.")
+    public var shouldHidePlaceholderText: Bool {
+        get {
+            return shouldHideToolbarPlaceholder
+        }
+        set(newValue) {
+            shouldHideToolbarPlaceholder = newValue
+        }
+    }
+    
+    /**
+     `toolbarPlaceholder` to override default `placeholder` text when drawing text on toolbar.
+     */
+    public var toolbarPlaceholder: String? {
+        get {
+            let aValue = objc_getAssociatedObject(self, &kIQToolbarPlaceholder) as? String
+            
+            return aValue
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &kIQToolbarPlaceholder, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            
+            self.keyboardToolbar.titleBarButton.title = self.drawingToolbarPlaceholder
+        }
+    }
+
+    @available(*,deprecated, message: "This is renamed to `toolbarPlaceholder` for more clear naming.")
+    public var placeholderText: String? {
+        get {
+            return toolbarPlaceholder
+        }
+        set(newValue) {
+            toolbarPlaceholder = newValue
+        }
+    }
+    
+    /**
+     `drawingToolbarPlaceholder` will be actual text used to draw on toolbar. This would either `placeholder` or `toolbarPlaceholder`.
+     */
+    public var drawingToolbarPlaceholder: String? {
+
+        if (self.shouldHideToolbarPlaceholder)
+        {
+            return nil
+        }
+        else if (self.toolbarPlaceholder?.isEmpty == false) {
+            return self.toolbarPlaceholder
+        }
+        else if self.responds(to: #selector(getter: UITextField.placeholder)) {
+            
+            if let textField = self as? UITextField {
+                return textField.placeholder
+            } else if let textView = self as? IQTextView {
+                return textView.placeholder
+            } else {
+                return nil
+            }
+        }
+        else {
+            return nil
+        }
+    }
+
+    @available(*,deprecated, message: "This is renamed to `drawingToolbarPlaceholder` for more clear naming.")
+    public var drawingPlaceholderText: String? {
+        
+        return drawingToolbarPlaceholder
+    }
+    
+    ///---------------------
+    /// MARK: Private helper
+    ///---------------------
+    
+    fileprivate static func flexibleBarButtonItem () -> IQBarButtonItem {
+        
+        struct Static {
+            static let nilButton = IQBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        }
+        
+        Static.nilButton.isSystemItem = true
+        return Static.nilButton
+    }
+
+    ///------------
+    /// MARK: Done
+    ///------------
+    
+    /**
+    Helper function to add Done button on keyboard.
+    
+    @param target Target object for selector.
+    @param action Done button action name. Usually 'doneAction:(IQBarButtonItem*)item'.
+    */
+    public func addDoneOnKeyboardWithTarget(_ target : AnyObject?, action : Selector) {
+        
+        addDoneOnKeyboardWithTarget(target, action: action, titleText: nil)
+    }
+
+    /**
+    Helper function to add Done button on keyboard.
+    
+    @param target Target object for selector.
+    @param action Done button action name. Usually 'doneAction:(IQBarButtonItem*)item'.
+    @param titleText text to show as title in IQToolbar'.
+    */
+    public func addDoneOnKeyboardWithTarget (_ target : AnyObject?, action : Selector, titleText: String?) {
+        
+        //If can't set InputAccessoryView. Then return
+        if self.responds(to: #selector(setter: UITextField.inputAccessoryView)) {
+            
+            //  Creating a toolBar for phoneNumber keyboard
+            let toolbar = self.keyboardToolbar
+            
+            var items : [IQBarButtonItem] = []
+            
+            //Flexible space
+            items.append(UIView.flexibleBarButtonItem())
+
+            //Title button
+            toolbar.titleBarButton.title = shouldHideToolbarPlaceholder == true ? nil : titleText
+            
+            #if swift(>=3.2)
+                if #available(iOS 11, *) {}
+                else {
+                    toolbar.titleBarButton.customView?.frame = CGRect.zero
+                }
+            #else
+                toolbar.titleBarButton.customView?.frame = CGRect.zero
+            #endif
+
+            items.append(toolbar.titleBarButton)
+            
+            //Flexible space
+            items.append(UIView.flexibleBarButtonItem())
+
+            //Done button
+            var doneButton = toolbar.doneBarButton
+            if doneButton.isSystemItem == false {
+                doneButton = IQBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: target, action: action)
+                doneButton.isSystemItem = true
+                doneButton.invocation = toolbar.doneBarButton.invocation
+                doneButton.accessibilityLabel = toolbar.doneBarButton.accessibilityLabel
+                toolbar.doneBarButton = doneButton
+            }
+
+            items.append(doneButton)
+            
+            //  Adding button to toolBar.
+            toolbar.items = items
+            
+            //  Setting toolbar to keyboard.
+            if let textField = self as? UITextField {
+                textField.inputAccessoryView = toolbar
+
+                switch textField.keyboardAppearance {
+                case UIKeyboardAppearance.dark:
+                    toolbar.barStyle = UIBarStyle.black
+                default:
+                    toolbar.barStyle = UIBarStyle.default
+                }
+            } else if let textView = self as? UITextView {
+                textView.inputAccessoryView = toolbar
+
+                switch textView.keyboardAppearance {
+                case UIKeyboardAppearance.dark:
+                    toolbar.barStyle = UIBarStyle.black
+                default:
+                    toolbar.barStyle = UIBarStyle.default
+                }
+           }
+        }
+    }
+    
+    /**
+    Helper function to add Done button on keyboard.
+    
+    @param target Target object for selector.
+    @param action Done button action name. Usually 'doneAction:(IQBarButtonItem*)item'.
+    @param shouldShowPlaceholder A boolean to indicate whether to show textField placeholder on IQToolbar'.
+    */
+    public func addDoneOnKeyboardWithTarget (_ target : AnyObject?, action : Selector, shouldShowPlaceholder: Bool) {
+        
+        var title : String?
