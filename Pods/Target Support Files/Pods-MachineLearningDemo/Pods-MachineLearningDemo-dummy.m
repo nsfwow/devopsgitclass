bd2ef54bcf30f42ab64ddc9fb4113032861bfcbd
nsfwow <116341914+nsfwow@users.noreply.github.com>
2023-03-22 14:18:21 +0000

    typo;

--- Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-dummy.m
+++ Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-dummy.m
@@ -1,2 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MachineLearningDem
+@interface PodsDummy_Pods_MachineLearningDemo : NSObject
+@end
+@implementation PodsDummy_Pods_MachineLearningDemo
+@end
