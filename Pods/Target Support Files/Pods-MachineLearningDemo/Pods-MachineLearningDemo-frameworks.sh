bffbf2618612d49ff37f24868fb8e6c3369b8ca2
nsfwow <116341914+nsfwow@users.noreply.github.com>
2023-03-23 08:53:34 +0000

    Remove unnecessary files

--- Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-frameworks.sh
+++ Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-frameworks.sh
@@ -8,4 +8,11 @@ SWIFT_STDLIB_PATH="${DT_TOOLCHAIN_DIR}/usr/lib/swift/${PLATFORM_NAME}"
 
 # This protects against multiple targets copying the same framework dependency at the same time. The solution
 # was originally proposed here: https://lists.samba.org/archive/rsync/2008-February/020158.html
-RSYNC_
+RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
+
+install_framework()
+{
+  if [ -r "${BUILT_PRODUCTS_DIR}/$1" ]; then
+    local source="${BUILT_PRODUCTS_DIR}/$1"
+  elif [ -r "${BUILT_PRODUCTS_DIR}/$(basename "$1")" ]; then
+    local source="${BUILT_PRODUC
