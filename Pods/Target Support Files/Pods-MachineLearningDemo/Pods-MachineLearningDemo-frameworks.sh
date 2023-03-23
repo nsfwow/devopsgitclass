c69b8c11a588dcbe2a459726c2213c943c17baf2
nsfwow <116341914+nsfwow@users.noreply.github.com>
2023-03-23 06:29:18 +0000

    UI fixes

--- Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-frameworks.sh
+++ Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-frameworks.sh
@@ -4,4 +4,8 @@ set -e
 echo "mkdir -p ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 mkdir -p "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
-SWIFT_STDLIB_PATH="${DT_TOOLCHA
+SWIFT_STDLIB_PATH="${DT_TOOLCHAIN_DIR}/usr/lib/swift/${PLATFORM_NAME}"
+
+# This protects against multiple targets copying the same framework dependency at the same time. The solution
+# was originally proposed here: https://lists.samba.org/archive/rsync/2008-February/020158.html
+RSYNC_
