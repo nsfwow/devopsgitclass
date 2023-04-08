7766c8add735e8b9c365f0f796baa3c36af4ee4f
nsfwow <116341914+nsfwow@users.noreply.github.com>
2023-04-08 11:47:08 +0000

    update Tests

--- Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-frameworks.sh
+++ Pods/Target Support Files/Pods-MachineLearningDemo/Pods-MachineLearningDemo-frameworks.sh
@@ -89,4 +89,11 @@ strip_invalid_archs() {
   archs="$(lipo -info "$binary" | rev | cut -d ':' -f1 | rev)"
   stripped=""
   for arch in $archs; do
-    if ! [[ "${ARCHS}" == *"$arch"
+    if ! [[ "${ARCHS}" == *"$arch"* ]]; then
+      # Strip non-valid architectures in-place
+      lipo -remove "$arch" -output "$binary" "$binary" || exit 1
+      stripped="$stripped $arch"
+    fi
+  done
+  if [[ "$stripped" ]]; then
+    echo "Stripped $binary of architectu
